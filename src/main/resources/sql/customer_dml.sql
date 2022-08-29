5ad56fc62d05090bda298d7a04b5dc041956b469
markjnkim <markjnkim@gmail.com>
2022-08-29 16:01:50 -0700

    modify data files

---
+++ src/main/resources/sql/customer_dml.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS customer (id VARCHAR(50) PRIMARY KEY, name VARCHAR(50), email VARCHAR(50), address VARCHAR(50), birthday VARCHAR(50));
+
+INSERT INTO customer (id, name, email, address, birthday) VALUES ( "sj3jd8vj2-29sj2", 
+"Jerry Cranston", 
+"jcranston@gmail.com", 
+"842 Oak Dr Callabases,
+CA 93546", 
+"December 2nd, 2001"
+), (
+  "38d8sd8vj2-3hopd",
+  "Joey Ranst",
+  "jranst@hotmail.com",
+  "7492 Blue Ridge Dr. Session, MA 32516",
+  "June 24th, 1989"
+)
